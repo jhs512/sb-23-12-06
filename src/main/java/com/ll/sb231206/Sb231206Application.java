9e21c4eab715c48a12be5b1ff14e986d4367b773
jhs512 <jangka512@gmail.com>
2023-12-06 15:20:22 +0900

    work

---
+++ src/main/java/com/ll/sb231206/Sb231206Application.java
@@ -0,0 +1,13 @@
+package com.ll.sb231206;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb231206Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb231206Application.class, args);
+    }
+
+}
